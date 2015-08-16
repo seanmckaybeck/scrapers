33da89be21144f336cb76aca7fde72c5c39022cd
Sean Beck <seanmckaybeck@gmail.com>
2015-08-15 23:09:37 -0400

    craigslist free section scrapers

---
+++ craigslist/free_stuff_requests_parallel.py
@@ -0,0 +1,58 @@
+import argparse
+import concurrent.futures
+from multiprocessing import cpu_count
+import time
+
+import requests
+from bs4 import BeautifulSoup
+
+
+FREE_QUERY = '/search/zip'
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-w', type=str, default='http://baltimore.craigslist.org', help='your local craigslist website, including HTTP schema')
+    return parser.parse_args()
+
+
+def extract_entry_data(entry):
+    link = entry.select('a')[0]['href']
+    submitted = entry.select('time')[0]['datetime']
+    title = entry.find_all('a', class_='hdrlnk')[0].text
+    return {'link': link, 'submitted': submitted, 'title': title}
+
+
+def parse(url):
+    u = '{}{}'.format(url, FREE_QUERY)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:39.0) Gecko/20100101 Firefox/39.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate',
+        'DNT': '1',
+        'Cookie': 'cl_tocmode=sss%3Agrid'
+    }
+    start = time.time()
+    resp = requests.get(u, headers=headers)
+    print('Took {} seconds to retrieve page'.format(time.time()-start))
+    soup = BeautifulSoup(resp.content, 'html.parser')
+    entries = soup.select('p.row')
+    count = cpu_count()
+    data = []
+    start = time.time()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=count*2) as executor:
+        res = [executor.submit(extract_entry_data, entry) for entry in entries]
+        for future in concurrent.futures.as_completed(res):
+            try:
+                entry = future.result()
+                data.append(entry)
+            except Excecption as e:
+                print(str(e))
+    print('Took {} seconds to retrieve item details'.format(time.time()-start))
+    [print(entry) for entry in data]
+
+
+if __name__ == '__main__':
+    args = get_args()
+    parse(args.w)
---
+++ craigslist/free_stuff_scrapy.py
@@ -0,0 +1,51 @@
+import pymongo
+from scrapy.spiders import Spider
+from scrapy.item import Item, Field
+from scrapy.exceptions import DropItem
+
+
+class Entry(Item):
+    title = Field()
+    url = Field()
+    submitted = Field()
+
+
+class MongoPipeline(object):
+    def __init__(self, server, port, db, collection):
+        connection = pymongo.MongoClient(
+                server, port
+                )
+        db = connection[db]
+        self.collection = db[collection]
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        settings = crawler.settings
+        return cls(settings.get('MONGODB_SERVER'),
+                   settings.get('MONGODB_PORT'),
+                   settings.get('MONGODB_DB'),
+                   settings.get('MONGODB_COLLECTION'))
+
+    def process_item(self, item, spider):
+        self.collection.update({'url': item['url']}, dict(item), upsert=True)
+
+
+class CraigslistSpider(Spider):
+    name = 'craigslist'
+    allowed_domains = ['craigslist.org']
+    start_urls = ['http://baltimore.craigslist.org/search/zip']
+    custom_settings = {
+        'MONGODB_SERVER': 'localhost',
+        'MONGODB_PORT': 27017,
+        'MONGODB_DB': 'craigslist',
+        'MONGODB_COLLECTION': 'free',
+        'ITEM_PIPELINES': {'free_stuff_scrapy.MongoPipeline': 100}
+    }
+
+    def parse(self, response):
+        for sel in response.xpath('//div[@class="content"]/p'):
+            item = Entry()
+            item['url'] = sel.xpath('a/@href').extract()[0]
+            item['submitted'] = sel.xpath('span/span/time/@datetime').extract()[0]
+            item['title'] = sel.xpath('span/span/a[@class="hdrlnk"]/text()').extract()[0]
+            yield item

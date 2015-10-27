8b6e3c0e6809587940f9dde86ef76c70b6bb3e96
Sean Beck <seanmckaybeck@gmail.com>
2015-10-27 16:58:24 +0000

    extract_ids.py now removes duplicates
    Fixes #1

--- boardgamegeek/extract_ids.py
+++ boardgamegeek/extract_ids.py
@@ -5,5 +5,6 @@ filename = 'items.csv'
 reader = csv.reader(open(filename))
 col = reader.next().index('id')
 ids = [line[col] for line in reader]
+ids = list(set(ids))
 with open('ids.txt', 'w') as f:
     f.write('\n'.join(ids))
